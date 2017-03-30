a746bddf0ddea412dc33440d56b99056e79b94f9
Ron Dobley <ron@rondobley.com>
2017-03-30 15:57:24 -0700

    fix footer link to github

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                     <div className='row'>
                         <div className='col-sm-12'>
                             <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
-                            <p><strong>Open Source</strong> code available at <Link to="https://github.com/rondobley/meal-planner">Github</Link></p>
+                            <p><strong>Open Source</strong> code available at <a href="https://github.com/rondobley/meal-planner">Github</a></p>
                         </div>
                     </div>
                 </div>
